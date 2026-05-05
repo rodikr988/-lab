edb1b7384b35e4d2533a36bbb1cd5dac38556a7a
rodikr988 <rodikr988@gmail.com>
2026-05-05 10:57:16 +0300

    Add files via upload

---
+++ lab2/lab2.0.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Student {
+public:
+    string firstName;
+    string lastName;
+    string group;
+    int age;
+
+    Student(string fName, string lName, string grp, int a) {
+        firstName = fName;
+        lastName = lName;
+        group = grp;
+        age = a;
+    }
+
+    void printInfo() const {
+        cout << firstName << " " << lastName
+            << " | Група: " << group
+            << " | Вік: " << age << endl;
+    }
+};
+
+int main() {
+    string filename = "students.txt";
+    vector<Student> studentVector;
+    ifstream inFile(filename);
+
+    if (inFile.is_open()) {
+        string fName, lName, grp;
+        int a;
+
+        while (inFile >> fName >> lName >> grp >> a) {
+            studentVector.push_back(Student(fName, lName, grp, a));
+        }
+        inFile.close();
+    }
+    else {
+        cout << "Помилка відкриття файлу!" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < studentVector.size(); i++) {
+        studentVector[i].printInfo();
+    }
+
+    return 0;
+}
